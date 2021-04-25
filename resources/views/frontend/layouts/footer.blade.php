c1e517db56d83d0cc5c28998f374d4964d0e5ed7
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-04-26 02:04:41 +0600

    some changes on footer

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="" > 
-  <div class="text-center mt-3"><code class="text-light">Made with <3 by <a style="color: #eebc1f" href="https://shakilahmed.info/  ">shakil_ahmed</a></code></div>
+  <div class="text-center mt-3"><code class="text-light">Coded with <3 by <a style="color: #eebc1f" href="https://shakilahmed.info/  ">shakil_ahmed</a></code></div>
   <div class="text-center"><code><h2 style="color: #ffffff"><b>Arigato!</b></h2></code></div>
 </footer>
